815f7e5e8f02f604fb2ed4b4042ca37797083b64
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-03-11 22:29:21 +0545

    login/signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_msngr/loginpage.dart';
 import 'package:frontend_msngr/messagepage.dart';
 import 'homepage.dart';
 
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginPage(),
     );
   } 
 }
---
+++ lib/signup.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:frontend_msngr/loginpage.dart';
+
+class SignupPage extends StatelessWidget {
+  SignupPage({super.key});
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  // void dispose(){ 
+  //   usernameController.dispose();
+  //   passwordController.dispose();
+  //   super.dispose();
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Container(
+        margin: const EdgeInsets.only(top: 60),
+        height: MediaQuery.of(context).size.height * 0.7,
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              const Text("SIGNUP",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  )),
+              // ignore: prefer_const_constructors
+              TextField(
+                controller: usernameController,
+                decoration: const InputDecoration(labelText: "enter username"),
+              ),
+              // ignore: prefer_const_constructors
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(labelText: "enter password"),
+              ),
+              const SizedBox(height: 20),
+               ElevatedButton(
+                style: ButtonStyle(
+                  elevation: MaterialStatePropertyAll(1) 
+                ),
+                  onPressed: (){
+                    print(usernameController.text);
+                    print(passwordController.text);
+                  },
+                  child: Text("Submit", style: TextStyle(color: Colors.white)))
+                  ,
+              const SizedBox(height: 20),
+               InkWell(
+               
+                  onTap: (){
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>LoginPage()));
+                  },
+                  child: Text("Login Instead", style: TextStyle(color: Colors.black)))
+            ]),
+      ),
+    ));
+  }
+}
